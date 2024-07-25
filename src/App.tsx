3382058309f5bb92084a786dc52aade21679b017
DmitryUgryumov <dmitryugryumov13@gmail.com>
2024-07-25 11:22:45 +0600

    change content

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis dignissimos dolore, eligendi error expedita laborum magni porro. Aspernatur magni, quasi. At, cupiditate dolor ipsam molestiae quas repudiandae vel voluptatum.
+    123
     </div>
   );
 }
