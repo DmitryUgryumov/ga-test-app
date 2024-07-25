ef1fd939cbb510c40c6d04f7a3e8500d31f2ac65
DmitryUgryumov <dmitryugryumov13@gmail.com>
2024-07-25 11:11:54 +0600

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis dignissimos dolore, eligendi error expedita laborum magni porro. Aspernatur magni, quasi. At, cupiditate dolor ipsam molestiae quas repudiandae vel voluptatum.
     </div>
   );
 }
